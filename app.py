870beefc8004c306312378d01a2239e378cb1562
manishsingh26 <manishsinghjss@gmail.com>
2022-09-19 20:42:08 +0530

    port change

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    APP.run(host='0.0.0.0', port=os.environ.get('listenport', 8080))
+    APP.run(host='0.0.0.0', port=os.environ.get('listenport', 8020))
